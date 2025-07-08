abdfad54a358de8275763f4849ba4f12ca71726c
MR-VL <cosmmo6@gmail.com>
2025-07-08 16:48:45 -0500

    [Success] config files compiled [Missing] Python pip install yt-dlp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,24 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
+        maven {
+            url = uri("https://chaquo.com/maven")
+        }
     }
 }
 
-rootProject.name = "yt-dlp-android"
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://chaquo.com/maven")
+        }
+    }
+}
+
+rootProject.name = "yt-dlp-Android"
 include(":app")
